0474d761dda137f7c2724669ac5c1e4b417eb3de
Reza Farhadi <mor.farhadi@gmail.com>
2016-03-22 16:09:43 -0400

    Removing AES encryption/decryption

--- src/main/java/epay/web/EriPayController.java
+++ src/main/java/epay/web/EriPayController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,15 +13,13 @@ import com.paypal.base.rest.PayPalRESTException;
 
 import epay.domain.Item;
 import epay.domain.Purchase;
-import epay.service.EncryptionService;
 import epay.service.ItemService;
 import epay.service.PriceService;
 
 @RestController
 public class EriPayController {
 
-  @Autowired
-  private EncryptionService encryptionService;
+  public static final String FRONTEND_ENDPOINT = "http://localhost:3000";
 
   @Autowired
   private ItemService itemService;
@@ -30,31 +27,16 @@ public class EriPayController {
   @Autowired
   private PriceService priceService;
 
-  @CrossOrigin(origins = "http://localhost:3000")
+  @CrossOrigin(origins = FRONTEND_ENDPOINT)
   @RequestMapping(value = "/items", method = RequestMethod.GET)
   public List<Item> getItems() {
     return itemService.getItems();
   }
 
-  @CrossOrigin(origins = "http://localhost:3000")
+  @CrossOrigin(origins = FRONTEND_ENDPOINT)
   @RequestMapping(value = "/price", method = RequestMethod.POST)
   public String postPurchase(@RequestBody final Purchase purchase) throws PayPalRESTException {
     return priceService.postPur(purchase);
   }
 
-  // encrypt a string
-  @RequestMapping("/encryption")
-  public String encrypt(
-        @RequestHeader(value = "text", required = true) String textToEncrypt,
-        @RequestHeader(value = "secretKey", required = true) String secretKey) {
-    return encryptionService.getEncryptedWithKey(textToEncrypt, secretKey);
-  }
-
-  // decrypt a string
-  @RequestMapping(value = "/decryption", method = RequestMethod.GET)
-  public String decrpty(
-        @RequestHeader(value = "text", required = true) String textToDecrypt,
-        @RequestHeader(value = "secretKey", required = true) String secretKey) {
-    return encryptionService.getDecryptedWithKey(textToDecrypt, secretKey);
-  }
 }
